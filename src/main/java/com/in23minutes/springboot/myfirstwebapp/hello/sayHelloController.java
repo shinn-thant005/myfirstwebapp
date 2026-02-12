e7b3677bfeac40fdaecd181a22c495e6646e1b76
Shinn Thant <sthant497@gmail.com>
2026-02-13 01:06:35 +0630

    Initial commit

---
+++ src/main/java/com/in23minutes/springboot/myfirstwebapp/hello/sayHelloController.java
@@ -0,0 +1,31 @@
+package com.in23minutes.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class sayHelloController {
+    @RequestMapping("/say-hello")
+    @ResponseBody
+    public String sayHello(){
+        return "Hello World";
+    }
+
+    @RequestMapping("/say-hello-html")
+    @ResponseBody
+    public String sayHelloHTML(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("<html>");
+        sb.append("<head>");
+        sb.append("<title>Hello World</title>");
+        sb.append("</head>");
+        sb.append("<body>");
+        sb.append("<h1>Hello World</h1>");
+        sb.append("</body>");
+        sb.append("<html>");
+
+        return "Hello World";
+    }
+}
