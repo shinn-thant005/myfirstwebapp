d72b32b7857f780a12321a9b96bc1189548de1e7
Shinn Thant <sthant497@gmail.com>
2026-02-14 01:58:25 +0630

    idk just add

--- src/main/java/com/in23minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in23minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,7 +1,10 @@
 package com.in23minutes.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -11,8 +14,15 @@ public class LoginController {
         return "login";
     }
 
-    @RequestMapping("student-login")
-    public String goTostudentLoginPage() {
+    @RequestMapping(value="student-login", method = RequestMethod.GET)
+    public String goToStudentLoginPage() {
         return "student-login";
+    }
+
+    @RequestMapping(value="student-login", method = RequestMethod.POST)
+    public String goToWelcomePage(@RequestParam String name, @RequestParam String password, ModelMap model){
+        model.put("name", name);
+        model.put("password", password);
+        return "welcome-page";
     }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome-page.jsp
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Welcome Page</title>
+    </head>
+    <body>
+        <div>Welcome to in28minutes</div>
+        <div>Username: ${name}</div>
+        <div>Password: ${password}</div>
+    </body>
+</html>
